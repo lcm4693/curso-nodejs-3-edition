7770340779ac8c4eb475030b9768600d034e927b
Diego Serpa <diego.neves@serpro.gov.br>
2019-12-06 17:37:56 -0300

    Tudo ate Aula 14

--- node-course/notes-app/app.js
+++ node-course/notes-app/app.js
@@ -1,8 +1,14 @@
 const validator = require('validator')
 const getNotes = require('./notes.js')
 
+const chalk = require('chalk')
+
 const msg = getNotes()
 console.log(msg)
 
 console.log(validator.isEmail('diego@example.com'))
 console.log(validator.isURL('http://www.uol.com.br'))
+
+console.log(chalk.green.bold.inverse('Success'))
+
+console.log('Teste do Nodemon')
