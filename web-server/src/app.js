2858de4499f6c3bcbe6b3294157729eceb067c34
Diego Serpa <diegos.java@gmail.com>
2019-12-13 20:01:27 -0300

    Routes on Express - Aula 43

---
+++ web-server/src/app.js
@@ -0,0 +1,24 @@
+const express = require('express');
+
+const app = express();
+
+
+app.get('', (request, response) => {
+    response.send('Hello Express!')
+})
+
+app.get('/help', (request, response) => {
+    response.send('Help page!')
+})
+
+app.get('/about', (request, response) => {
+    response.send('About Page!')
+})
+
+app.get('/weather', (request, response) => {
+    response.send('Weather page!')
+})
+
+app.listen(3000, () => {
+    console.log('Server is up on port 3000.');
+})
