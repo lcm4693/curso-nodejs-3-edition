138f7f972b004038d065dea836db1406a18c4832
Diego Serpa <diegos.java@gmail.com>
2019-12-14 00:14:29 -0300

    Serving HTML e JSON - Aula 44

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -4,19 +4,29 @@ const app = express();
 
 
 app.get('', (request, response) => {
-    response.send('Hello Express!')
+    response.send('<h1>Weather</h1>')
 })
 
 app.get('/help', (request, response) => {
-    response.send('Help page!')
+    response.send([{
+        name: 'Diego',
+        age: 33
+    },
+    {
+        name: 'Daniele',
+        age: 32
+    }])
 })
 
 app.get('/about', (request, response) => {
-    response.send('About Page!')
+    response.send('<h1>Diego</h1> <h2>Daniele</h2>')
 })
 
 app.get('/weather', (request, response) => {
-    response.send('Weather page!')
+    response.send({
+        forecast: '50 graus',
+        location: 'Rio de Janeiro'
+    })
 })
 
 app.listen(3000, () => {
