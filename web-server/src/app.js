8d3506b9b1fa3004e71e935b488aa51a3e5546c3
Diego Serpa <diegos.java@gmail.com>
2019-12-14 00:39:13 -0300

    Serving pages HTML - Aula 45

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,26 +1,34 @@
+const path = require('path')
 const express = require('express');
+
+// console.log(__dirname)
+// console.log(__filename)
 
 const app = express();
 
+const publicDirectoryPath = path.join(__dirname, '../public')
 
-app.get('', (request, response) => {
-    response.send('<h1>Weather</h1>')
-})
+app.use(express.static(publicDirectoryPath))
 
-app.get('/help', (request, response) => {
-    response.send([{
-        name: 'Diego',
-        age: 33
-    },
-    {
-        name: 'Daniele',
-        age: 32
-    }])
-})
+//Após colocar o arquivo index.html, ele é chamado automaticamente
+//através do express.static
+// app.get('', (request, response) => {
+//     response.send('<h1>Weather</h1>')
+// })
 
-app.get('/about', (request, response) => {
-    response.send('<h1>Diego</h1> <h2>Daniele</h2>')
-})
+// app.get('/help', (request, response) => {
+//     response.send([{
+//         name: 'Diego',
+//         age: 33
+//     },
+//     {
+//         name: 'Daniele',
+//         age: 32
+//     }])
+// })
+// app.get('/about', (request, response) => {
+//     response.redirect('about.html')
+// })
 
 app.get('/weather', (request, response) => {
     response.send({
