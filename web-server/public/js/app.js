b5212484999a94ced6d4864f59663f3a5ff6133c
Diego Serpa <diegos.java@gmail.com>
2019-12-18 20:22:30 -0300

    HTTP Request with Fetch - Aula 57

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -1,1 +1,11 @@
 console.log('Client side Javascript file is loaded!')
+
+fetch('http://localhost:3000/weather?address=Paris, França').then((response) => {
+    response.json().then((data) => {
+        if(data.error){
+            return console.log(data.error)
+        }
+        console.log(data.forecast);
+        console.log(data.location);
+    });
+})
