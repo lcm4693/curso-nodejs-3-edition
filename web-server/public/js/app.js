b3021240fe143393d444d3b2b2be549b6105e783
Diego Serpa <diegos.java@gmail.com>
2019-12-18 20:36:07 -0300

    Creating a search form - Aula 58

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -1,11 +1,22 @@
 console.log('Client side Javascript file is loaded!')
 
-fetch('http://localhost:3000/weather?address=Paris, França').then((response) => {
-    response.json().then((data) => {
-        if(data.error){
-            return console.log(data.error)
-        }
-        console.log(data.forecast);
-        console.log(data.location);
+
+
+const weatherForm = document.querySelector('form');
+const search = document.querySelector('input');
+weatherForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    const location = search.value
+    
+    fetch('http://localhost:3000/weather?address=' + location).then((response) => {
+        response.json().then((data) => {
+            if(data.error){
+                return console.log(data.error);
+            }
+            console.log(data.forecast);
+            console.log(data.location);
+        });
     });
+
 })
